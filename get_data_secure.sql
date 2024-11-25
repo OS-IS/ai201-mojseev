e2fe0d5f9831b4327b8eb2de16fd2be36181d67b
Mykola <nmoiseiev@gmail.com>
2024-11-25 17:00:52 +0200

    Delete get_data_secure.sql

--- get_data_secure.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION change_data(attr1_value TEXT, attr2_value INT)
-RETURNS VOID AS $$
-BEGIN
-    UPDATE human
-    SET permission = attr2_value
-    WHERE human.name = attr1_value; 
-END;
-$$ LANGUAGE plpgsql;
-
